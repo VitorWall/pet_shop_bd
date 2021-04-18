4daeb07319d1d70caf7cce113a08e1c3d7fbf349
VitorWall <vitor@DESKTOP-2FTQ6HA.localdomain>
2021-04-17 23:47:21 -0300

    Método de editar Unidade

--- pet_shop_bd/sistema_pet_shop/urls.py
+++ pet_shop_bd/sistema_pet_shop/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = "sistema_pet_shop"
 
 urlpatterns = [
-    path('', views.Home.as_view(), name="home"),
+    path('', views.Unidades.as_view(), name="home"),
     path('nova-unidade', views.novaUnidade, name="novaUnidade"),
+    path('editar-unidade/<int:pk>', views.editUnidade, name="novaUnidade"),
 ]
