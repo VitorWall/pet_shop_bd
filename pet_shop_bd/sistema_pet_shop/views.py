966242dbe5981f372e7da13e13b09a539ffda9f4
VitorWall <vitor@DESKTOP-2FTQ6HA.localdomain>
2021-04-17 22:38:45 -0300

    Criar o arquivo base.html e a homepage

--- pet_shop_bd/sistema_pet_shop/views.py
+++ pet_shop_bd/sistema_pet_shop/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect, get_object_or_404
+from django.views.generic import DeleteView, ListView
+from .models import *
 
 # Create your views here.
+class Home (ListView):
+    model = Unidade
+
